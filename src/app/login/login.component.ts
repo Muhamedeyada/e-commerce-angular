235c48bc01b6180a9732899e4cce348e61bcf18f
eyada <mohamed.ayada.ma@gmail.com>
2024-07-30 00:20:28 +0300

    handle login and sign up

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,11 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule, RouterLink],
+  imports: [FormsModule, RouterLink, NgIf],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
